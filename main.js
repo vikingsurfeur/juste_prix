e27a12c15df11479d6babaa316375c6fc1e187d5
David Bouscarle <d.bouscarle@gmail.com>
2021-01-28 14:35:10 +0100

    submit + coups

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 let input       = document.querySelector('#prix'),
     error       = document.querySelector('small'),
-    formulaire  = document.querySelector('#formulaire');
+    formulaire  = document.querySelector('#formulaire'),
+    coups       = 0;
 
 // Etape 2 - Cacher l'erreur
 
@@ -15,7 +16,7 @@ let nombreAleatoire = Math.floor(Math.random() * 1001);
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
 
 input.addEventListener('keyup', () => {
-    if (isNaN(input.value)) {
+    if (isNaN(input.value) || input.value == "") {
         error.style.display = 'inline';
     } else {
         error.style.display = 'none';
@@ -25,7 +26,13 @@ input.addEventListener('keyup', () => {
 // Etape 5 - Agir à l'envoi du formulaire
 
 formulaire.addEventListener('submit', (e) => {
-    e.preventDefault;
+    e.preventDefault();
+    if (isNaN(input.value) || input.value == "") {
+        input.style.borderColor = "red";
+    } else {
+        coups++;
+        input.style.borderColor = "silver";
+    }
 })
 
 // Etape 6 - Créer la fonction vérifier
